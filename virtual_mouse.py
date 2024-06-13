5f65b7a67c0199cacd4f33025dfd747c1a2041a3
Irash Perera <irash.21@cse.mrt.ac.lk>
2024-06-14 01:00:41 +0530

    Initial Commit

---
+++ virtual_mouse.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+import hand_tracker as ht
+import time
+import autopy as ap
+
+cam_width, cam_height = 640, 480
+
+cap = cv2.VideoCapture(0)
+cap.set(3, cam_width)
+cap.set(4, cam_height)
+
+while True:
+    ret, img = cap.read()
+    
+    cv2.imshow('Image', img)
+    cv2.waitKey(1)
